84e27e10f29068030839bc7bddc7cbf23e95c719
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2023-06-08 13:48:59 +0530

    initial setup for mfe

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const ele = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(ele);
 root.render(
   <React.StrictMode>
     <App />
